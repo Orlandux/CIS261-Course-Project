c73c5d1a0c24866194c44f9c1f69d611184226d4
username <useremail>
2026-02-07 08:32:38 -0800

    Phase 2

--- CIS261_Course_Project.py
+++ CIS261_Course_Project.py
@@ -4,6 +4,10 @@
 def get_employee_name():
     name = input("Enter employee name, or type 'End' to quit: ")
     return name 
+def get_dates():
+    start_date = input("Enter a start date (mm/dd/yyyy): ")
+    end_date = input("Enter an end date (mm/dd/yyyy): ")
+    return start_date, end_date
 def get_total_hours(): 
     hours = float(input("Enter total hours worked: ")) 
     return hours 
@@ -21,7 +25,9 @@ def calculate_pay(hours, rate, tax_rate):
     return gross_pay, income_tax, net_pay
 
 
-def show_employee_info(name, hours, rate, gross_pay, tax_rate, income_tax, net_pay):
+def show_employee_info(start_date, end_date, name, hours, rate, gross_pay, tax_rate, income_tax, net_pay):
+    print("Start Date:", start_date)
+    print("End Date:", end_date)
     print("Employee Name:", name)
     print("Total Hours:", hours)
     print("Hourly Rate: $", rate)
@@ -30,37 +36,66 @@ def show_employee_info(name, hours, rate, gross_pay, tax_rate, income_tax, net_p
     print("Income Tax : $", income_tax)
     print("Net Pay: $", net_pay)
 
-def show_totals(total_employees, total_hours, total_gross, total_tax, total_net):
-    print("Total Employees:", total_employees)
-    print("Total Hours:", total_hours)
-    print("Total Gross Pay: $:", total_gross)
-    print("Total Tax: $", total_tax)
-    print("Total Net Pay: $", total_net)
+def show_totals(totals_dict):
+    print("Total Employees:", totals_dict["total_employees"])
+    print("Total Hours:", totals_dict["total_hours"])
+    print("Total Gross Pay: $", totals_dict["total_gross"])
+    print("Total Tax: $", totals_dict["total_tax"])
+    print("Total Net Pay: $", totals_dict["total_net"])
 
-total_employees = 0
-total_hours_all = 0.0
-total_gross_all = 0.0
-total_tax_all = 0.0
-total_net_all = 0.0 
 
+
+def process_employees(employee_list):
+    totals_dict = {
+        "total_employees": 0,
+        "total_hours": 0.0,
+        "total_gross": 0.0,
+        "total_tax": 0.0,
+        "total_net": 0.0
+    }
+
+    for emp in employee_list:
+        start_date = emp[0]
+        end_date = emp[1]
+        name = emp[2]
+        hours = emp[3]
+        rate = emp[4]
+        tax_rate = emp[5]
+
+        gross_pay, income_tax, net_pay = calculate_pay(hours, rate, tax_rate)
+
+        show_employee_info(
+            start_date,
+            end_date,
+            name,
+            hours,
+            rate,
+            gross_pay,
+            tax_rate,
+            income_tax,
+            net_pay
+        )
+
+        totals_dict["total_employees"] += 1 
+        totals_dict["total_hours"] += hours
+        totals_dict["total_gross"] += gross_pay
+        totals_dict["total_tax"] += income_tax
+        totals_dict["total_net"] += net_pay
+    return totals_dict 
+
+employee_list = []
 while True:
     employee_name = get_employee_name()
     if employee_name.lower() == "end":
         break 
 
+    start_date, end_date = get_dates()
     hours = get_total_hours()
     rate = get_hourly_rate()
     tax_rate = get_tax_rate()
 
-    gross_pay, income_tax, net_pay = calculate_pay(hours, rate, tax_rate)
-    show_employee_info(employee_name, hours, rate, gross_pay, tax_rate, income_tax, net_pay)
+    employee_list.append([start_date, end_date, employee_name, hours, rate, tax_rate])
 
-    total_employees += 1 
-    total_hours_all += hours 
-    total_gross_all += gross_pay
-    total_tax_all += income_tax
-    total_net_all += net_pay
-
-show_totals(total_employees, total_hours_all, total_gross_all, total_tax_all, total_net_all)
-
+totals = process_employees(employee_list)
+show_totals(totals)
 
